a265d070896a1bed94dc4aaee4e5584ad258cab8
moep0 <43595537+moep0@users.noreply.github.com>
2020-09-01 16:00:11 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sed '/cda/c192.168.136.128 cda' /etc/hosts -i
-../home/hduser/bigdata/spark-2.4.5/sbin/start-all.sh
+cd /home/hduser/bigdata/spark-2.4.5/sbin
+./start-all.sh
 
 export PYSPARK_DRIVER_PYTHON=jupyter
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --ip=0.0.0.0"
