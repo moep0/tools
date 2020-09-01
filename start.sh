17d7c7d67a7c8541fc1a1850666297cca2345d9e
moep0 <43595537+moep0@users.noreply.github.com>
2020-09-01 14:55:26 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-sed '/cda/c192.168.136.128 cda' /etc/hosts
-~/bigdata/spark-2.4.6/sbin/start-all.sh
+sed '/cda/c192.168.136.128 cda' /etc/hosts -i
+../home/hduser/bigdata/spark-2.4.5/sbin/start-all.sh
 
 export PYSPARK_DRIVER_PYTHON=jupyter
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --ip=0.0.0.0"
 pyspark --master spark://cda:7077
-
